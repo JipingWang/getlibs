8e542a6a3585adb432dd3f5a82dc011e34ddb253
Jiping Wang <jwang@healthinteractiveinc.com>
2019-11-14 18:36:41 -0500

    ...

--- demos/react/index.js
+++ demos/react/index.js
@@ -5,11 +5,13 @@ window.addEventListener('DOMContentLoaded', event => {
     console.warn('no appUrl')
     return
   }
-
-  let props = typeof appProps === 'undefined' ? {} : appProps
+/**@ts-ignore */
+  let props = (typeof appProps === 'undefined') ? {} : appProps
 
   Promise.all(['react', 'react-dom', appUrl].map(x => SystemJS.import(x))).then(modules => {
     const [React, ReactDOM, AppModule] = modules
+    const config = SystemJS.getConfig()
+    console.info({React: React.version, SystemJS: System.version, TypeScript: config.map.typescript, config,})
     ReactDOM.render(React.createElement(AppModule.default), document.querySelector('#root'))
   })
 })
--- examples/react/es6/App.tsx
+++ examples/react/es6/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 export function App() {
+  const [state, setState] = React.useState()
   return (
     <div>
       <header>React using systemjs</header>
